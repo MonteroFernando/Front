5df8fc89032285a8d5ccd660f8bccc600dd9b9ba
Fer_Stock <monterofernandoe@gmail.com>
2023-08-07 20:23:15 -0300

    comienzo con js

---
+++ JS/Ejercicio1.js
@@ -0,0 +1,8 @@
+function sumar(nro1,nro2){
+    let resultado=nro1+nro2;
+    return(resultado);
+}
+
+let resultado=sumar(1,2);
+
+console.log(resultado)
---
+++ JS/Ejercicio2.js
@@ -0,0 +1,10 @@
+function ordenar(array){
+    let ordenado=array.tosorted();
+    return ordenado
+}
+
+
+let lista=[5,6,8,9,7,10,5];
+
+
+console.log(ordenar(lista));
