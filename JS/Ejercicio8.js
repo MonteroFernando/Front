3c8d855f994c7029afae53f88369f2db3ddea0ba
Fer_Stock <monterofernandoe@gmail.com>
2023-08-09 13:22:08 -0300

    Agregado de Doscstring del Ej8

--- JS/Ejercicio8.js
+++ JS/Ejercicio8.js
@@ -4,35 +4,38 @@ const persona={nombre:"Fernando",
              sexo:"Masculino",
              telefono:3876842177
             }
-
+///Creacion de los elementos
 const contenedor=document.createElement("div");
 const tabla=document.createElement("table");
+///Fila del eencabezado
 const fila_e=document.createElement("tr");
+//Encabezados
 const enc_nombre=document.createElement("th");
 const enc_apellido=document.createElement("th");
 const enc_edad=document.createElement("th");
 const enc_sexo=document.createElement("th");
 const enc_telefono=document.createElement("th");
-const dato=document.createElement("td");
+///Fila 1 para datos
 const fila_d1=document.createElement("tr");
+///Datos de la fila1
 const nombre1=document.createElement("td");
 const apellido1=document.createElement("td");
 const edad1=document.createElement("td");
 const sexo1=document.createElement("td");
 const telefono1=document.createElement("td");
-
+///Ingreso los nombres del encabezado
 enc_nombre.textContent="Nombre";
 enc_apellido.textContent="Apellido";
 enc_edad.textContent="Edad";
 enc_sexo.textContent="Sexo";
 enc_telefono.textContent="Telefono";
-
+///Ingreso los datos del objeto personal para la primer fila de datos
 nombre1.textContent=persona.nombre;
 apellido1.textContent=persona.apellido;
 edad1.textContent=persona.edad;
 sexo1.textContent=persona.sexo;
 telefono1.textContent=persona.telefono;
-
+///Estructuracion de los hijos de los elementos
 contenedor.appendChild(tabla);
 tabla.appendChild(fila_e);
 fila_e.appendChild(enc_nombre);
@@ -46,7 +49,7 @@ fila_d1.appendChild(apellido1);
 fila_d1.appendChild(edad1);
 fila_d1.appendChild(sexo1);
 fila_d1.appendChild(telefono1);
-
+///Ingreso del contenedor en el documento
 document.body.appendChild(contenedor);
 
 
